e16937a9b443e8e638168079817d1ea943eaedab
Konfus-dev <jerclimber12@gmail.com>
2023-07-17 20:54:11 -0400

    Singleton no longer executes in edit mode (no idea why it did in the first place)

--- Runtime/Utility/Design Patterns/Singleton.cs
+++ Runtime/Utility/Design Patterns/Singleton.cs
@@ -2,7 +2,6 @@ using UnityEngine;
 
 namespace Konfus.Utility.Design_Patterns
 {
-    [ExecuteInEditMode]
     /// <summary>
     /// Inherit from if you want a singleton GameObject.
     /// </summary>
