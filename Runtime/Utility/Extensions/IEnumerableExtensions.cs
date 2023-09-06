3515242cdcaed899bbdc42494473fb2df55898db
Konfus-dev <63468146+Konfus-dev@users.noreply.github.com>
2023-09-06 15:55:42 -0700

    Moved HandlesExtensions.cs and added IEnumerableExtensions.cs

---
+++ Runtime/Utility/Extensions/IEnumerableExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Unity.Mathematics;
+
+namespace Konfus_Systems_Tools_n_Utils_Package.Runtime.Utility.Extensions
+{
+    public static class IEnumerableExtensions
+    {
+        public static T GetRandom<T>(this IEnumerable<T> enumerable)
+        {
+            var array = enumerable.ToArray();
+            var enumCount = array.Count();
+            var rand = Random.CreateFromIndex((uint)enumCount);
+            return array.ElementAt(rand.NextInt(enumCount));
+        }
+    }
+}
