2ff2d60449b056ec804c3d16b459f22a8f67d17d
konfus-dev <jerclimber12@gmail.com>
2023-11-17 21:01:56 -0800

    Adding new effects to fx system.

---
+++ Runtime/Systems/FX/EnableDisableColliderEffect.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+
+namespace Konfus.Systems.FX
+{
+    [Serializable]
+    public class EnableDisableColliderEffect : Effect
+    {
+        [SerializeField]
+        private Collider collider;
+
+        [SerializeField] 
+        private bool enabled;
+        
+        public override void Play()
+        {
+            collider.enabled = enabled;
+        }
+    }
+}
---
+++ Runtime/Systems/FX/SetRigidBodyIsKinematicEffect.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+
+namespace Konfus.Systems.FX
+{
+    [Serializable]
+    public class SetRigidBodyIsKinematicEffect : Effect
+    {
+        [SerializeField]
+        private Rigidbody rigidbody;
+
+        [SerializeField] 
+        private bool isKinematic;
+
+        public override void Play()
+        {
+            rigidbody.isKinematic = isKinematic;
+        }
+    }
+}
