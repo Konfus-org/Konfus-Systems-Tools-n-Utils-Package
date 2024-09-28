29cae9147bdd5e332524d2fd0603266c16f55db8
Konfus-dev <63468146+Konfus-dev@users.noreply.github.com>
2024-09-28 13:11:41 -0700

    Update MinMax.cs

--- Runtime/Utility/Custom Types/MinMax.cs
+++ Runtime/Utility/Custom Types/MinMax.cs
@@ -1,6 +1,9 @@
-﻿namespace Konfus.Utility.Custom_Types
+﻿using System;
+using UnityEngine;
+
+namespace Konfus.Utility.Custom_Types
 {
-    [System.Serializable]
+    [Serializable]
     public struct MinMaxFloat
     {
         public MinMaxFloat(float minimum, float maximum)
